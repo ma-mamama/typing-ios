c2f0bfbea70ec16eb866afd45e35ae454150db1b
ma-mamama <mamamamam.2525@gmail.com>
2025-08-28 02:14:42 +0900

    Initial Commit

---
+++ AItest/AItestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AItestApp.swift
+//  AItest
+//
+//  Created by masato on 2025/08/28.
+//
+
+import SwiftUI
+
+@main
+struct AItestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AItestTests/AItestTests.swift
@@ -0,0 +1,17 @@
+//
+//  AItestTests.swift
+//  AItestTests
+//
+//  Created by masato on 2025/08/28.
+//
+
+import Testing
+@testable import AItest
+
+struct AItestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
